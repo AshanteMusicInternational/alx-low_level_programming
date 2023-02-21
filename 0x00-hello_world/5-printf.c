0e589dc51ca357eecb9a3092531b5e3d3eef063b
Ashante Music <ashantecharlotte@gmail.com>
2023-02-20 16:46:29 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program to print
+ * with proper gramar, but the outcome is a piece of art,
+ * using printf function
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
