38f4a2f9cc754275cd9abd7e2d19464f65f0da6e
Ashante Music <ashantecharlotte@gmail.com>
2023-02-20 17:11:24 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: c program to print the size of datatypes
+ * using function sizeof();
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char a;
+int b;
+float c;
+long d;
+long long e;
+
+printf("Size of a char: %zu byte(s)\n", sizeof(a));
+printf("Size of an int: %zu byte(s)\n", sizeof(b));
+printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of a float: %zu byte(s)\n", sizeof(c));
+
+return (0);
+}
