cf9a8d47af59a969caee659cd78aa7414fa682cf
Ashante Music <ashantecharlotte@gmail.com>
2023-02-21 11:49:08 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * Description: prints if a number is  positive or negative
+ * Description: test for  positive or negative
  *
  * Return: Always 0 (Success)
  *
@@ -14,14 +14,14 @@ int main(void)
 {
 	int n;
 
-	strand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* check if the number is negative */
 	if (n < 0)
-		print("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	else if (n > 0)
-		printf("%d is positive\n" n);
+		printf("%d is positive\n", n);
 	else
-		printf("%d is zero\n" n);
+		printf("%d is zero\n", n);
 	return (0);
 }
