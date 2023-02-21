dd7abb49c014cf47cef47886272763e0ea237595
Ashante Music <ashantecharlotte@gmail.com>
2023-02-20 16:24:22 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
- /**
-  * main - main entry point
-  * return : always 0 (success)
-  */
+
+/**
+ * main - Entry point
+ *
+ * Description: write a program file that will print
+ * exactly "programming is like building a multilingual puzzle"
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
