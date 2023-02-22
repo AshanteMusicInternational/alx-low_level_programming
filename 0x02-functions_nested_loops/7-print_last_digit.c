f4b630bcc2ee937e566ef2e30180ee20c1030543
Ashante Music <ashantecharlotte@gmail.com>
2023-02-22 13:38:34 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code.
+ * @i: test parameter
+ * Return: Always 0.
+ */
+int print_last_digit(int i)
+{
+	int k;
+
+	if (i < 0)
+		k = -(i % 10);
+	else
+		k = (i % 10);
+	_putchar('0' + k);
+	return (k);
+}
