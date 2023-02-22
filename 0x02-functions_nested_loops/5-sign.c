f8b011f82df7eaf6a02e85d3c92c21d8f723478a
Ashante Music <ashantecharlotte@gmail.com>
2023-02-22 12:55:21 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code.
+ * @n: test parameter
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		i = 1;
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		i = -1;
+	}
+	else
+	{
+		_putchar('0');
+		i = 0;
+	}
+	return (i);
+}
+
