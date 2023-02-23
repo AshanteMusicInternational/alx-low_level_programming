2b503c2abe326905477a089ef555b22b1d76b502
Ashante Music <ashantecharlotte@gmail.com>
2023-02-22 18:54:01 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#incude <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Check Main 
+ * @n: A input integer
+ * Description: function that prints all natura numbers
+ * from n to 98, followed by a new line.
+ * Return: Nothing
+ *
+ print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+	
